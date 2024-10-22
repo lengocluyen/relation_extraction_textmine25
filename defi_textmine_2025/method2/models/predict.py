d968f12d24a401cc8bb8a1d432fe8aca10aae277
Tagny G <tagny@ymail.com>
2024-10-22 07:47:22 +0200

    #7 predict.py: a final version

--- defi_textmine_2025/method2/models/predict.py
+++ defi_textmine_2025/method2/models/predict.py
@@ -3,7 +3,12 @@ python -m defi_textmine_2025.method2.models.predict
 """
 
 import logging
+import os
 from defi_textmine_2025.method2.data.loading import load_data
+from defi_textmine_2025.method2.data.relation_and_entity_classes import (
+    RELATION_CLASSES,
+    WITH_RELATION_CLASS,
+)
 from defi_textmine_2025.settings import (
     INTERIM_DIR,
     OUTPUT_DIR,
@@ -47,7 +52,9 @@ def predict(task_name, step_name, model_class, num_fold, train_df, val_df, input
     logging.info(f"{target_columns=}")
     ismultilabel = task2step2ismultilabel[task_name][step_name]
     logging.info(f"{ismultilabel=}")
-    logging.warning(f"Preparing data for the sub-task {task_name} and {step_name=}...")
+    logging.warning(
+        f"Preparing data for the sub-task `{task_name}` and {step_name=}..."
+    )
     task = Task.init_predefined_sub_task(
         task_name, pd.concat([train_df, val_df], axis=0)
     )
@@ -72,26 +79,35 @@ def predict(task_name, step_name, model_class, num_fold, train_df, val_df, input
     )
     pred_onehot_df = pd.DataFrame(
         data=pred_onehot, columns=target_columns, index=task_input_data.index
-    ).add_suffix(f"{task_name}-{step_name}")
+    )  # .add_suffix(f"{task_name}-{step_name}")
     pred_probas_df = (
         pd.DataFrame(
             data=pred_probas, columns=target_columns, index=task_input_data.index
-        )
-        .add_suffix("_proba")
-        .add_suffix(f"-{task_name}-{step_name}")
+        ).add_suffix("-proba")
+        # .add_suffix(f"-{task_name}-{step_name}")
     )
     return pd.concat([pred_onehot_df, pred_probas_df], axis=1)
 
 
 if __name__ == "__main__":
+    predictions_dir = f"{OUTPUT_DIR}/method2-cv5/fold1"
     num_fold = 1  # int(sys.argv[1])
-    input_path = "data/defi-text-mine-2025/interim/reduced_text_w_entity_bracket/test"  # int(sys.argv[2])
-    input_df = load_data(input_path, index_col=0).head(1000)
-    logging.info(f"{input_df.shape=}")
+    # input_path = "data/defi-text-mine-2025/interim/reduced_text_w_entity_bracket/test"  # int(sys.argv[2])
+    input_path = "data/defi-text-mine-2025/interim/method2-5_fold_cv/validation-fold1-mth2.parquet"  # int(sys.argv[2])
+    data = load_data(input_path, index_col=0)  # .head(1000)
+    logging.info(f"{data.shape=}")
+    # pipelines = [
+    #     [("roottask", "RI"), ("subtask1", "RC")],
+    #     [("roottask", "RI"), ("subtask2", "RC")],
+    #     [("roottask", "RI"), ("subtask3", "RC")],
+    # ]
     pipelines = [
-        [("roottask", "RI"), ("subtask1", "RC")],
-        [("roottask", "RI"), ("subtask2", "RC")],
-        [("roottask", "RI"), ("subtask3", "RC")],
+        [
+            ("roottask", "RI"),
+            ("subtask1", "RC"),
+            ("subtask2", "RC"),
+            ("subtask3", "RC"),
+        ],
     ]
     logging.warning(f"Prediction with {pipelines=} - [{num_fold=}]")
     model_class: BertBasedModel = BertMlp
@@ -103,14 +119,23 @@ if __name__ == "__main__":
     for pipeline in pipelines:
         logging.warning(f"Applying {pipeline=}")
         for task_name, step_name in pipeline:
-            # if "RC" in step_name:
-            #     # we assume NO_RELATION_CLASS and WITH_RELATION_CLASS are columns
-            #     # of input_data
-            #     with_rel_data = task_input_data[]
             y_pred2d = predict(
-                task_name, step_name, model_class, num_fold, train_df, val_df, input_df
+                task_name, step_name, model_class, num_fold, train_df, val_df, data
             )
-            input_df.loc[y_pred2d.index, y_pred2d.columns] = y_pred2d
-            save_data(input_df, f"{OUTPUT_DIR}/test_pred.csv", with_index=False)
-            break
-        break
+            data.loc[y_pred2d.index, y_pred2d.columns] = y_pred2d
+            save_data(
+                data,
+                f"{predictions_dir}/{os.path.basename(input_path)}_pred-before-filtering.csv",
+                with_index=False,
+            )
+
+
+data = data[[col for col in data if not col.endswith("proba")]].fillna(0.0)
+w_rel_data = data[data[WITH_RELATION_CLASS] == 1.0]
+no_rel_data = data[data[WITH_RELATION_CLASS] == 0]
+no_rel_data.loc[:, RELATION_CLASSES] = 0.0
+save_data(
+    pd.concat([w_rel_data, no_rel_data], axis=0),
+    f"{predictions_dir}/{os.path.basename(input_path)}_pred-after-filtering.csv",
+    with_index=False,
+)
